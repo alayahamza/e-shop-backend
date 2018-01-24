525a75cf90e8a1dab6ca491e0ddbda7f44a3cd56
hamzaalaya <alaya.hamza@gmail.com>
2018-01-24 17:27:53 +0100

    update UT

--- src/test/java/com/eshop/service/CategoryServiceTest.java
+++ src/test/java/com/eshop/service/CategoryServiceTest.java
@@ -6,10 +6,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.jdbc.EmbeddedDatabaseConnection;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -42,5 +39,6 @@ public class CategoryServiceTest {
     @Test
     public void findAll() {
 //        List<Category> categories = categoryRepository.findAll();
+//        Assert.assertEquals(categories.size(), 4);
     }
 }
